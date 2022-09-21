be6e8f05c3080e6fd927671c56afba28097c7ed9
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-21 14:42:48 -0600

    second

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 app.use('/', express.static(path.join(__dirname, '..client/index.html')))
-app.use(express.static(path.join(__dirname, '..client')))
+app.use(express.static(path.join(__dirname, '../client')))
 
 
 
