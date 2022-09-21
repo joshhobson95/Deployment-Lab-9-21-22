e73f26d8a382f3adfbf1730dc768a2111b261705
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-21 14:35:03 -0600

    first

---
+++ client/main.js
@@ -0,0 +1,1 @@
+console.log('YOURE DOING GREAT')
---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 5000
+const app = express()
+
+require('dotenv').config()
+
+app.use(express.json())
+
+
+
+app.use('/', express.static(path.join(__dirname, '..client/index.html')))
+app.use(express.static(path.join(__dirname, '..client')))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log('Docked at port' + port)
+})
