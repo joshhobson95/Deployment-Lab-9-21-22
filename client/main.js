55dc2c03758e2aec92707a06f224de21bc02b82c
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-22 15:46:42 -0600

    b

--- client/main.js
+++ client/main.js
@@ -1,10 +1,17 @@
-
+const {rollbar} = require
 
 function myFunction() {
   var element = document.getElementById("snom");
   element.classList.toggle("grayscale");
   console.log('click');
-  rollbar.log("User clicked");
+
+  try {
+    nonExistentFunction2();
+  } catch (error) {
+    rollbar.log('User Clicked');
+   
+  };
+ 
 }
 
 let snomclick = document.querySelector("#snom");
@@ -14,16 +21,5 @@ snomclick.addEventListener("click", myFunction);
 
 console.log('YOURE DOING GREAT')
 
-//live server and the way you have to write your srcs WILL SCREW THIS ENTIRE THING UP!!!!!
-
-//FIXED !!!
-
-// <!-- <script src="/js"></script> -->
-// <script src="main.js"></script>
-
-// </body>
-// had to comment out the first script 
-//and change the second script to main.js with no slash
-//not sure if this will work when something is deployed but it works now!!!
 
 
