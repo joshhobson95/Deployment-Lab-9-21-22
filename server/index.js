4f523883c7fb02e5954cfa3d232ba63695ffc0fb
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-22 14:52:29 -0600

    twenty

--- server/index.js
+++ server/index.js
@@ -7,6 +7,27 @@ require('dotenv').config()
 
 app.use(express.json())
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '54fe487c727144589f10dcffd524cc68',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    rollbar.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  };
+
+
+
 
 
 app.use('/', express.static(path.join(__dirname, '..client/index.html')))
