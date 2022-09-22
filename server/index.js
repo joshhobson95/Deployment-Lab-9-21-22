2c235eb3c8c46a1f0827eeb661ee7fb94c1f15d7
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-22 14:58:49 -0600

    thirtenn

--- server/index.js
+++ server/index.js
@@ -26,6 +26,15 @@ try {
     // Note - error messages will vary depending on browser
   };
 
+  try {
+    nonExistentFunction2();
+  } catch (error) {
+    rollbar.critical(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  };
+
+
 
 
 
