5fef9dd56725c91d0e6244613263bf2fd0cafe7d
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-22 14:44:36 -0600

    twel

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@
 function myFunction() {
   var element = document.getElementById("snom");
   element.classList.toggle("grayscale");
+  console.log('click')
 }
 
 let snomclick = document.querySelector("#snom");
@@ -12,3 +13,17 @@ snomclick.addEventListener("click", myFunction);
 
 
 console.log('YOURE DOING GREAT')
+
+//live server and the way you have to write your srcs WILL SCREW THIS ENTIRE THING UP!!!!!
+
+//FIXED !!!
+
+// <!-- <script src="/js"></script> -->
+// <script src="main.js"></script>
+
+// </body>
+// had to comment out the first script 
+//and change the second script to main.js with no slash
+//not sure if this will work when something is deployed but it works now!!!
+
+
