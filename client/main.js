259216bc7eb5a7e1af4b8f2db1ff81acc81efef8
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-21 19:25:54 -0600

    ele

--- client/main.js
+++ client/main.js
@@ -1,4 +1,14 @@
 
 
 
+function myFunction() {
+  var element = document.getElementById("snom");
+  element.classList.toggle("grayscale");
+}
+
+let snomclick = document.querySelector("#snom");
+snomclick.addEventListener("click", myFunction);
+
+
+
 console.log('YOURE DOING GREAT')
